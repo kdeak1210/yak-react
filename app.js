32a3c6a85259bc3558434bc793677fbcbc2fb965
kyle <kdeak1210@gmail.com>
2017-09-18 12:29:33 -0400

    Manually set up react in the application
    Used handlebars as the template engine for express, and am rendering the default layout page 'main.handlebars' as the homepage at '/'. Here is where React links into the app. Webpack is set up for react and es2015, and outputs bundle.js into public/build/

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const routes = require('./routes/index');
+// Bring in our routes (index and API)
 const api = require('./routes/api');
 
+// Initialize the express app
 const app = express();
 
-const dbUrl = 'mongodb://localhost/yikkish';
+// Set the view engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
+// Set up and connect to mongodb through mongoose
+const dbUrl = 'mongodb://localhost/yikkish';
 mongoose.connect(dbUrl, { useMongoClient: true });
 
 mongoose.connection.on('connected', () => {
@@ -22,21 +28,25 @@ mongoose.connection.on('error', (err) => {
   console.log('Database error: ' + err);
 });
 
+// Set up some middleware - morgan, body + cookie parsers, static files
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Serve pages in public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', routes);
+// Set up root + API routes
+app.get('/', (req, res) => {
+  res.render('layouts/main');
+});
 app.use('/api', api);
 
-const port = process.env.port | 3000;
-
-app.get('/', (req, res) => {
-  res.send('hello');
-});
+const port = process.env.PORT | 3000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+module.exports = app;
