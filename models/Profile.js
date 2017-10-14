47a21afdc72e121c1ced3e43ab10c587ac5c719f
kyle <kdeak1210@gmail.com>
2017-10-13 22:11:36 -0400

    Heroku ready

--- models/Profile.js
+++ models/Profile.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const ProfileSchema = new mongoose.Schema({
   username: {type: String, default: ''},
   city: {type: String, default: ''},
+  image: {type: String, default: ''},
   gender: {type: String, default: ''},
   password: {type: String, default: ''},  
   timestamp: {type: Date, default: Date.now()}
-})
+});
 
 module.exports = mongoose.model('ProfileSchema', ProfileSchema);
